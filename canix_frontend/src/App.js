a3749bd2e2e268ab9b289536fb86f54f68aca2d8
samuel ch <samuel.dev.ch@gmail.com>
2024-02-20 06:21:31 -0800

    added react app

---
+++ canix_frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <>
+    <div className="bg-gray-100 h-screen p-8">
+      <h1 className="text-3xl font-bold text-indigo-600">
+        CANIX CODE EXERCISE
+      </h1>
+    </div>
+    </>
+  );
+};
+
+export default App;
---
+++ canix_frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './styles.css'],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
+
